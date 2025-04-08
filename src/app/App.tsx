3232059159dab32242ca3015bbfa34b6d0f8efdd
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-04-08 20:42:06 +0700

    add footer and btns style

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { Footer } from "../widgets/Footer/Footer";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ src/widgets/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { Btn } from "../../shared/Button/btn";
+import "./Footer.css";
+
+export function Footer() {
+    return (
+        <footer>
+            <Btn />
+            <Btn />
+        </footer>
+    );
+}
