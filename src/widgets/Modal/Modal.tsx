76fab735c2e78e95969dfecf59791733e7dacc3d
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-04-11 00:10:14 +0700

    add setting updating node

--- src/widgets/Modal/Modal.tsx
+++ src/widgets/Modal/Modal.tsx
@@ -1,20 +1,35 @@
+import { useState } from "react";
 import { ModalProps } from "../../classes/ModalProps";
 import { useTree } from "../../hooks/UseTree";
 import "./Modal.css";
 
 export function Modal({ type, setModalType }: ModalProps) {
     const { selectedNode, addNode, updateNode } = useTree();
+    const [nodeName, setNodeName] = useState("");
 
-    console.log(selectedNode);
+    const applyChanges = () => {
+        if (nodeName.length < 1) return;
+
+        if (type === "adding") addNode(selectedNode, nodeName, false);
+        else updateNode(selectedNode, nodeName);
+
+        setModalType("closed");
+        setNodeName("");
+    };
+
+    const disableApply = () => {
+        if (nodeName.length < 1) return true;
+        return false;
+    };
 
     return (
         <div
             className={`modal-window ${type === "closed" ? "hidden" : ""}`}
-            onClick={() => setModalType("closed")}
+            onMouseDown={() => setModalType("closed")}
         >
             <div
                 className="modal-window-body"
-                onClick={(e) => e.stopPropagation()}
+                onMouseDown={(e) => e.stopPropagation()}
             >
                 <div className="modal-window-body__head">
                     <button
@@ -24,9 +39,26 @@ export function Modal({ type, setModalType }: ModalProps) {
                         X
                     </button>
                 </div>
-                <div className="modal-window-body__form"></div>
+
+                <div className="modal-window-body__form">
+                    <input
+                        placeholder="Node name"
+                        autoComplete="off"
+                        value={nodeName}
+                        onChange={(val) => setNodeName(val.target.value)}
+                    />
+
+                    {type === "adding" ? "" : ""}
+                </div>
+
                 <div className="modal-window-body__bottom">
-                    <button className="apply-button">Apply</button>
+                    <button
+                        className="apply-button"
+                        onClick={() => applyChanges()}
+                        disabled={disableApply()}
+                    >
+                        Apply
+                    </button>
                 </div>
             </div>
         </div>
